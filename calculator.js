2c53136994b3d86a12982aaa542f2a1c3f4cf6df
Wendyy-J <109003556+Wendyy-J@users.noreply.github.com>
2022-09-17 01:27:37 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,6 @@
-// write a function to display values
+ // write a function to display values
  function display(val){
      document.getElementById('result').value += val
-
      return val
  }
 
@@ -10,10 +9,65 @@
      let x = document.getElementById('result').value
      let y = eval(x);
      document.getElementById('result').value = y
-
      return y
  }
 
  function clearscreen() {
      document.getElementById('result').value=''
+ }
+
+ function mathcos() {
+     let x = document.getElementById('result').value
+     let y = Math.cos(x);
+     document.getElementById('result').value= y
+     return y
+ }
+
+ function mathsin() {
+     let x = document.getElementById('result').value
+     let y = Math.sin(x);
+     document.getElementById('result').value= y
+     return y
+ }
+ 
+ function sqrt() {
+     let x = document.getElementById('result').value
+     let y = Math.sqrt(x);
+     document.getElementById('result').value= y
+     return y
+ }
+
+ function mathtan() {
+     let x = document.getElementById('result').value
+     let y = Math.tan(x);
+     document.getElementById('result').value= y
+     return y
+ }
+
+ function mathlog10() {
+     let x = document.getElementById('result').value
+     let y = Math.log10(x);
+     document.getElementById('result').value= y
+     return y
+ }
+
+ function pow() {
+     let x = document.getElementById('result').value
+     let y = Math.pow(x, 2);
+     document.getElementById('result').value= y
+     return y
+ }
+
+ function mathlog() {
+     let x = document.getElementById('result').value
+     let y = Math.log(x) ;
+     document.getElementById('result').value= y
+     return y
+ }
+
+ function exp() {
+     let x = document.getElementById('result').value
+     let y = Math.exp(x);
+     document.getElementById('result').value= y
+     return y
  }
